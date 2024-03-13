c797e2d9d403af6b919a90547a618ad6565b82a8
josubho <95227663+josubho@users.noreply.github.com>
2024-03-13 12:14:07 +0100

    solution to checpoint 7

---
+++ checkpoint-seven.js
@@ -0,0 +1,18 @@
+function sumaMult(numUno, numDos, numTres, numCuatro) {
+    sumaUno = numUno + numDos
+    sumaDos = numTres + numCuatro
+    multi = sumaUno * sumaDos
+    if(multi>50) {
+        console.log("¡El número es mayor que 50!")
+    } else if(multi<50) {
+        console.log("¡El número es menor que 50!")
+    }
+}
+
+sumaMult(1,1,1,1,)
+sumaMult(4,5,6,7)
+
+
+
+
+
